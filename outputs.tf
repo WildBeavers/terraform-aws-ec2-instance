7efb896106a81fe1cae326b6f7c6fe3a23999851
Philip Rosenberg <philip@rosenberg.dev>
2019-09-04 13:59:51 +0200

    Sort Locals and Outputs alphabetically

--- outputs.tf
+++ outputs.tf
@@ -7,27 +7,22 @@
     ######################################################################### */
 
 locals {
-  this_id                           = "${compact(coalescelist(aws_instance.this.*.id, [""]))}"
   this_availability_zone            = "${compact(coalescelist(aws_instance.this.*.availability_zone, [""]))}"
+  this_credit_specification         = "${flatten(aws_instance.this.*.credit_specification)}"
+  this_id                           = "${compact(coalescelist(aws_instance.this.*.id, [""]))}"
   this_key_name                     = "${compact(coalescelist(aws_instance.this.*.key_name, [""]))}"
-  this_public_dns                   = "${compact(coalescelist(aws_instance.this.*.public_dns, [""]))}"
-  this_public_ip                    = "${compact(coalescelist(aws_instance.this.*.public_ip, [""]))}"
+  this_password_data                = "${coalescelist(aws_instance.this.*.password_data, [""])}"
+  this_placement_group              = "${compact(coalescelist(aws_instance.this.*.placement_group, [""]))}"
   this_primary_network_interface_id = "${compact(coalescelist(aws_instance.this.*.primary_network_interface_id, [""]))}"
   this_private_dns                  = "${compact(coalescelist(aws_instance.this.*.private_dns, [""]))}"
   this_private_ip                   = "${compact(coalescelist(aws_instance.this.*.private_ip, [""]))}"
-  this_placement_group              = "${compact(coalescelist(aws_instance.this.*.placement_group, [""]))}"
+  this_public_dns                   = "${compact(coalescelist(aws_instance.this.*.public_dns, [""]))}"
+  this_public_ip                    = "${compact(coalescelist(aws_instance.this.*.public_ip, [""]))}"
   this_security_groups              = "${coalescelist(aws_instance.this.*.security_groups, [""])}"
-  this_vpc_security_group_ids       = "${coalescelist(flatten(aws_instance.this.*.vpc_security_group_ids), [""])}"
   this_subnet_id                    = "${compact(coalescelist(aws_instance.this.*.subnet_id, [""]))}"
-  this_credit_specification         = "${flatten(aws_instance.this.*.credit_specification)}"
   this_tags                         = "${coalescelist(aws_instance.this.*.tags, [""])}"
   this_volume_tags                  = "${coalescelist(aws_instance.this.*.volume_tags, [""])}"
-  this_password_data                = "${coalescelist(aws_instance.this.*.password_data, [""])}"
-}
-
-output "id" {
-  description = "List of IDs of instances"
-  value       = "${local.this_id}"
+  this_vpc_security_group_ids       = "${coalescelist(flatten(aws_instance.this.*.vpc_security_group_ids), [""])}"
 }
 
 output "availability_zone" {
@@ -35,9 +30,14 @@ output "availability_zone" {
   value       = "${local.this_availability_zone}"
 }
 
-output "placement_group" {
-  description = "List of placement groups of instances"
-  value       = "${local.this_placement_group}"
+output "credit_specification" {
+  description = "List of credit specification of instances"
+  value       = "${local.this_credit_specification}"
+}
+
+output "id" {
+  description = "List of IDs of instances"
+  value       = "${local.this_id}"
 }
 
 output "key_name" {
@@ -45,14 +45,14 @@ output "key_name" {
   value       = "${local.this_key_name}"
 }
 
-output "public_dns" {
-  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = "${local.this_public_dns}"
+output "password_data" {
+  description = "List of Base-64 encoded encrypted password data for the instance"
+  value       = "${local.this_password_data}"
 }
 
-output "public_ip" {
-  description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = "${local.this_public_ip}"
+output "placement_group" {
+  description = "List of placement groups of instances"
+  value       = "${local.this_placement_group}"
 }
 
 output "primary_network_interface_id" {
@@ -70,9 +70,14 @@ output "private_ip" {
   value       = "${local.this_private_ip}"
 }
 
-output "password_data" {
-  description = "List of Base-64 encoded encrypted password data for the instance"
-  value       = "${local.this_password_data}"
+output "public_dns" {
+  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
+  value       = "${local.this_public_dns}"
+}
+
+output "public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = "${local.this_public_ip}"
 }
 
 output "security_groups" {
@@ -80,19 +85,9 @@ output "security_groups" {
   value       = "${local.this_security_groups}"
 }
 
-output "vpc_security_group_ids" {
-  description = "List of associated security groups of instances, if running in non-default VPC"
-  value       = "${local.this_vpc_security_group_ids}"
-}
-
 output "subnet_id" {
   description = "List of IDs of VPC subnets of instances"
   value       = "${local.this_subnet_id}"
-}
-
-output "credit_specification" {
-  description = "List of credit specification of instances"
-  value       = "${local.this_credit_specification}"
 }
 
 output "tags" {
@@ -103,4 +98,9 @@ output "tags" {
 output "volume_tags" {
   description = "List of tags of volumes of instances"
   value       = "${local.this_volume_tags}"
+}
+
+output "vpc_security_group_ids" {
+  description = "List of associated security groups of instances, if running in non-default VPC"
+  value       = "${local.this_vpc_security_group_ids}"
 }
